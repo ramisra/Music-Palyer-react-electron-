56e3b647a8ba2e0f36dd4bddf59435a9871f28e9
ratikesh <ratikesh.limetray@gmail.com>
2016-05-25 10:24:56 +0545

    Finally Sound Cloud integrated

--- app/app.js
+++ app/app.js
@@ -1,29 +1,20 @@
+//React libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var React=require('react');
-var React_DOM=require('react-dom');
+//Import Container component
+import AppContainer from './containers/AppContainer'
 
-import Search from './components/search.component';
-import Details from './components/details.component';
-import Player from './components/player.component';
-import Progress from './components/progress.component';
-import Footer from './components/footer.component';
-
-
-
-var App = React.createClass({
-  render: function() {
-    return(
-        <div>
-        <Search />
-        <Details title={'Track Title'}/>
-        <Player/>
-        <Progress
-          position={'0.3'}
-          elapsed={'00:00'}
-          total={'0:40'}/>
-        <Footer/>
-        </div>
+class App extends React.Component {
+  render () {
+    return (
+      <AppContainer />
     );
   }
-});
-React_DOM.render(<App/>,document.getElementById('content'));
+}
+
+// Render to index.html
+ReactDOM.render(
+  <App />,
+  document.getElementById('content')
+);
